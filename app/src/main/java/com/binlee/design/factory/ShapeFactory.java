f05b7d2c77bce200276bddc378c3cd5c0d795849
sleticalboy <sleticalboy@gmail.com>
2020-10-13 21:42:51 +0800

    [feat]design-factory: doc and simple factory

---
+++ app/src/main/java/com/binlee/design/factory/ShapeFactory.java
@@ -0,0 +1,13 @@
+package com.binlee.design.factory;
+
+/**
+ * @author binlee sleticalboy@gmail.com
+ * created by IDEA on 2020/10/13
+ */
+public class ShapeFactory implements IFactory<IShape> {
+
+    @Override
+    public IShape create(String id) {
+        return null;
+    }
+}
