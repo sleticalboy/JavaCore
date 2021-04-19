7fc7c3f7e42e191fcbb717a159eefe4b39f64774
sleticalboy <sleticalboy@gmail.com>
2021-04-19 10:18:35 +0800

    [feat]Optimize multi threads download

--- lib-concurrence/src/main/java/com/binlee/thread/practice/Downloads.java
+++ lib-concurrence/src/main/java/com/binlee/thread/practice/Downloads.java
@@ -18,12 +18,11 @@ import java.util.concurrent.atomic.AtomicInteger;
  */
 public final class Downloads {
 
-    private final Logger logger;
+    private static final Logger sLogger = Logger.get(Downloads.class);
     private final String url;
     private final String file;
     private final int threads;
     private final AtomicInteger counter = new AtomicInteger(0);
-    private RandomAccessFile raf;
 
     private Downloads(String url, String file) {
         this(url, file, 3);
@@ -33,12 +32,12 @@ public final class Downloads {
         this.url = url;
         this.file = file;
         this.threads = threads;
-        this.logger = Logger.get(getClass());
     }
 
     public static void exec() {
         try {
-            String url = "https://github.com/agalwood/Motrix/releases/download/v1.5.15/Motrix_1.5.15_amd64.deb";
+            // String url = "https://github.com/agalwood/Motrix/releases/download/v1.5.15/Motrix_1.5.15_amd64.deb";
+            String url = "http://dldir1.qq.com/qqfile/qq/QQ7.9/16621/QQ7.9.exe";
             String file = FileUtil.getRootDir() + "/build/" + FileUtil.guessName(url);
             new Downloads(url, file, 3).start();
         } catch (IOException e) {
@@ -51,21 +50,25 @@ public final class Downloads {
         connection.setRequestMethod("GET");
         connection.setConnectTimeout(5000);
         final int code = connection.getResponseCode();
-        logger.i("start() response code: " + code);
+        sLogger.i("start() response code: " + code);
         if (code == HttpURLConnection.HTTP_OK) {
             final int length = connection.getContentLength();
-            logger.i("start() content length: " + length);
+            sLogger.i("start() content length: " + length);
             if (length > 5 * 1024 * 1024) {
-                // 多线程下载
-                raf = new RandomAccessFile(file, "rw");
+                // 获取文件长度
+                RandomAccessFile raf = new RandomAccessFile(file, "rw");
                 raf.setLength(length);
+                raf.close();
+                // 多线程下载
                 int start, end, chunk = length / threads;
                 for (int i = 0; i < threads; i++) {
                     start = i * chunk;
-                    end = i == threads - 1 ? (length - 1) : ((i + 1) * chunk);
-                    logger.i("start() download chunk i: " + i + ", start: " + start + ", end: " + end);
+                    end = i == threads - 1 ? (length - 1) : ((i + 1) * chunk - 1);
+                    sLogger.i("start() download chunk i: " + i + ", start: " + start + ", end: " + end);
                     new Thread(new Task(start, end, this, i)).start();
                 }
+            } else {
+                download("DirectDownload", 0, length);
             }
         }
     }
@@ -74,10 +77,12 @@ public final class Downloads {
         final HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
         connection.setRequestMethod("GET");
         connection.setConnectTimeout(5000);
-        connection.addRequestProperty("Range", "byte=" + start + "-" + end);
+        connection.setRequestProperty("Range", "bytes=" + start + "-" + end);
+        sLogger.i("download() start to get response code...");
         final int code = connection.getResponseCode();
-        logger.i("download() response code: " + code);
+        sLogger.i("download() response code: " + code);
         if (code == HttpURLConnection.HTTP_PARTIAL) {
+            RandomAccessFile raf = new RandomAccessFile(file, "rws");
             raf.seek(start);
             int read;
             final InputStream is = new BufferedInputStream(connection.getInputStream());
@@ -86,14 +91,14 @@ public final class Downloads {
                 raf.write(buffer, 0, read);
             }
             // toString() + "下载完成"
-            logger.i("download() " + tag + " completed");
+            sLogger.i("download() " + tag + " completed");
             is.close();
             final int mark = counter.incrementAndGet();
-            logger.i("download() completed threads: " + mark);
+            sLogger.i("download() completed threads: " + mark);
+            raf.close();
             if (mark == threads) {
-                raf.close();
                 // 所有的下载完成
-                logger.i("download() finished");
+                sLogger.i("download() finished");
             }
         }
     }
@@ -113,10 +118,11 @@ public final class Downloads {
 
         @Override
         public void run() {
+            sLogger.d("Task#run() " + this + ", start: " + start + ", end: " + end);
             try {
                 downloads.download(toString(), start, end);
             } catch (IOException e) {
-                e.printStackTrace();
+                sLogger.e("Task#run() error ", e);
             }
         }
     }
